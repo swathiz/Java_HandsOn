9a158651d3f7d86c3378f33cb5d8e5f53cf674fd
SWATHI <swathi@specrio.co>
2022-10-06 16:53:33 +0530

    ama

---
+++ AmazonEc2App/src/main/java/com/samples/demo/AmazonEc2App/App.java
@@ -0,0 +1,13 @@
+package com.samples.demo.AmazonEc2App;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
